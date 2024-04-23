e3bcaa1b622698542a8d2f963838ff1b80d29c80
Ali Astin Azarafza <astinazarafza@gmail.com>
2024-04-23 11:04:43 -0500

    its alive!

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ sB = sphere(pos=vector(-rB,0,0),radius=Rs, color=color.cyan)
 vA = sqrt(G*mB*rA)/(rA+rB)
 sA.p = mA*vA*vector(0,1,0)
 sB.p = -sA.p
+
+t = 0
+dt = 10000
+
+while t<5000*dt:
+    rate(100)
+    rAB = sB.pos - sA.pos
+    fBA = G*mA*mB*norm(rAB)/mag(rAB)**2
+    sA.p = sA.p + fBA*dt
+    sB.p = sB.p - fBA*dt
+    sA.pos = sA.pos + sA.p*dt/mA
+    sB.pos = sB.pos + sB.p*dt/mB
+    t = t + dt
